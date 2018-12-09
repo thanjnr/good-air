07a39bfd7aff14e6dd8ac1f31928d61c535207f7
Thomas Han <ethomas.han@gmail.com>
2018-12-09 17:41:50 +1300

    Add second template

--- templates/raleway.js
+++ templates/raleway.js
@@ -1,4 +1,4 @@
-var ArchivoTemplate = function (mesh) {
+var Raleway = function (mesh) {
     var mesh, texture, material, mainTagline, captionTagline, bgFillColor = "#078ac3", bgRectangle;
 
     init(mesh);
@@ -14,11 +14,9 @@ var ArchivoTemplate = function (mesh) {
 
     function init(meshToCopy) {
         var backgroundTexture = new BABYLON.DynamicTexture("dynamic texture", 1024, scene, true);
-        backgroundTexture.wAng = BABYLON.Tools.ToRadians(270) / backgroundTexture.uScale;
-        backgroundTexture.clearColor = new BABYLON.Color4(0, 0, 0, 0);
         
         bgRectangle = new Path2D();
-        bgRectangle.rect(0, 200, 1200, 122);
+        bgRectangle.rect(30, 200, 1200, 122);
 
         texture = backgroundTexture;
         updateTemplate();
@@ -41,36 +39,6 @@ var ArchivoTemplate = function (mesh) {
         material = dynamicMaterial;
     }
 
-    function createHorizontalCustomLabel(innerCylinder) {
-        var backgroundTexture = new BABYLON.DynamicTexture("dynamic texture", 1024, scene, true);
-
-        var rectangle = new Path2D();
-        rectangle.rect(30, 270, 1200, 70);
-        backgroundTexture.getContext().fillStyle = "#078ac3";
-        backgroundTexture.getContext().fill(rectangle);
-        backgroundTexture.update();
-
-        backgroundTexture.drawText("Enter text", 40, 330, "normal 72px Archivo Black", "white", null, true);
-        var dynamicMaterial = new BABYLON.StandardMaterial('mat', scene);
-        dynamicMaterial.diffuseTexture = backgroundTexture;
-        dynamicMaterial.opacityTexture = backgroundTexture;
-        // dynamicMaterial.diffuseTexture.invertZ = true;
-        // dynamicMaterial.diffuseTexture.vAng = 1;
-        // dynamicMaterial.diffuseTexture.wAng = BABYLON.Tools.ToRadians(90)/backgroundTexture.uScale;
-        dynamicMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-        dynamicMaterial.clearColor = new BABYLON.Color4(1, 0, 0, 0);
-        dynamicMaterial.backFaceCulling = false;
-
-        outerCylinder = innerCylinder.clone('outer');
-        outerCylinder.material = dynamicMaterial;
-
-        return {
-            cylinder: outerCylinder,
-            texture: backgroundTexture,
-            material: dynamicMaterial
-        };
-    }
-
     function addImage(image) {
         var backgroundTexture = texture;
 
@@ -81,42 +49,42 @@ var ArchivoTemplate = function (mesh) {
 
             img.onload = function () {
                 //Add image to dynamic texture
-                backgroundTexture.getContext().drawImage(this, 400, 820, 250, 175);
+                backgroundTexture.getContext().drawImage(this, 800, 380, 200, 175);
 
                  // get the image data object
-                 var image = backgroundTexture.getContext().getImageData(400, 820, 250, 175);
+                 var image = backgroundTexture.getContext().getImageData(800, 380, 200, 175);
                  // get the image data values 
                  var imageData = image.data,
                      length = imageData.length;
                  // set every fourth value to 50
                  for (var i = 3; i < length; i += 4) {
-                     imageData[i] = 30;
+                     imageData[i] = 50;
                  }
                  // after the manipulation, reset the data
                  image.data = imageData;
                  // and put the imagedata back to the canvas
-                 backgroundTexture.getContext().putImageData(image, 400, 820);
+                 backgroundTexture.getContext().putImageData(image, 800, 380);
 
                 backgroundTexture.update();
             }
         }
     }
 
-    function updateTemplate(mainText = "Enter text", caption = "Caption this", fillColor = "#078ac3") {
+    function updateTemplate(mainText = "Enter", caption = "Caption this", fillColor = "#078ac3") {
         var backgroundTexture = texture;
 
         bgFillColor = fillColor;
-        var rectangle = new Path2D();
-        rectangle.rect(0, 200, 1200, 122);
-        backgroundTexture.getContext().fillStyle = fillColor;
-        backgroundTexture.getContext().fill(rectangle);
-        backgroundTexture.update();
+        /* backgroundTexture.getContext().fillStyle = fillColor;
+        backgroundTexture.getContext().fill(bgRectangle);
+        backgroundTexture.update(); */
 
         mainTagline = mainText;
-        backgroundTexture.drawText(mainText.toUpperCase(), 250, 270, "70px 'Archivo Black'", "white", null, true);
+        backgroundTexture.drawText(mainText.toUpperCase(), 20, 430, "70px 'Archivo Black'", "white", null, true);
 
         captionTagline = caption;
-        backgroundTexture.drawText(caption.toUpperCase(), 250, 300, "32px 'Archivo Narrow'", "white", null, true);
+        backgroundTexture.drawText(caption.toUpperCase(), 20, 460, "32px 'Archivo Narrow'", "white", null, true);
+                
+        backgroundTexture.drawText('This is a very long paragraph', 20, 490, "16px 'Archivo Narrow'", "white", null, true);
 
         // rotate(outerCylinder);
     }
@@ -135,9 +103,11 @@ var ArchivoTemplate = function (mesh) {
         backgroundTexture.getContext().fill(rectangle);
         backgroundTexture.update();
         
-        backgroundTexture.drawText(mainTagline, 250, 220, "70px 'Archivo Black'", "white", null, true);
+        backgroundTexture.drawText(mainTagline, 20, 420, "70px 'Archivo Black'", "white", null, true);
 
-        backgroundTexture.drawText(captionTagline, 250, 250, "32px 'Archivo Narrow'", "white", null, true);
+        backgroundTexture.drawText(captionTagline, 20, 450, "32px 'Archivo Narrow'", "white", null, true);
+        
+        backgroundTexture.drawText('This is a very long paragraph', 20, 460, "16px 'Archivo Narrow'", "white", null, true);
 
         var img = new Image();
         img.src = logo;

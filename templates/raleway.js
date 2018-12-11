5bf3c06c8a01b8e8ec0842cad39a3c30d19f366d
Thomas Han <ethomas.han@gmail.com>
2018-12-11 23:30:28 +1300

    Fix styles of side bar

--- templates/raleway.js
+++ templates/raleway.js
@@ -1,6 +1,6 @@
 var Raleway = function (mesh) {
     var mesh, texture, material, topTagline, mainTagline, captionTagline, 
-        paragraphText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`, 
+        paragraphText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`, 
         bgFillColor = "#078ac3",
         bgRectangle;
 
